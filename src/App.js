a55d77a94500ddc5f362b8f8cd4849682961ac29
Norpa1 <norpa344@gmail.com>
2020-08-14 13:02:35 +0300

    Update App.js
    And now this outputting only needed data =D

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ var NewTrancastionsArray = data.results
 
 
 function TableFilters(o) {
- 
+ 	
 	var today = new Date();
 
 	var Condition1 = false
@@ -19,47 +19,63 @@ function TableFilters(o) {
 	var Condition4 = false
 	var WholeCondition = ""
 
+	var ConditionText1 = ' (NewTrancastionsArray[x].type=="Доход") '
+	var ConditionText2 = ' (NewTrancastionsArray[x].type=="Расход") '
+	var ConditionText3 = ' (ComparableDate.getMonth()==(today.getMonth()-1)) ' 
+	var ConditionText4 = ' (NewTrancastionsArray[x].value>1000) '
+
  	if (o.target.className == "Button") {o.target.setAttribute("class", "ButtonActive")}
 	else if (o.target.className == "ButtonActive") {o.target.setAttribute("class", "Button")}
 
  	var NonActiveFilters = document.getElementsByClassName("Button")
  	var ActiveFilters = document.getElementsByClassName("ButtonActive")
 
+ 	NewTrancastionsArray = data.results
+
  	for (var i=0;i<ActiveFilters.length;i++) {
 
  	if (ActiveFilters[i].innerHTML == Filters[0]) {
  		var Condition1 = true 
- 		WholeCondition = WholeCondition +' (NewTrancastionsArray[x].type=="Доход") ' 
+ 		WholeCondition = WholeCondition +ConditionText1
  	}
 
  	if (ActiveFilters[i].innerHTML == Filters[1]) {
  		var Condition2 = true	
-		WholeCondition = WholeCondition +' (NewTrancastionsArray[x].type=="Расход") ' 
+		WholeCondition = WholeCondition +ConditionText2
  	}
+ 
  	if (ActiveFilters[i].innerHTML == Filters[2]) {
  		var Condition3 = true	
-		WholeCondition = WholeCondition +' (ComparableDate.getMonth()==(today.getMonth()-1)) ' 
- 	}
- 	if (ActiveFilters[i].innerHTML == Filters[3]) {
- 		var Condition4 = true	
-		WholeCondition = WholeCondition + " NewTrancastionsArray[x].value>1000 "
+		WholeCondition = WholeCondition +ConditionText3
  	}
 
+ 	if (ActiveFilters[i].innerHTML == Filters[3]) {
+ 		var Condition4 = true	
+		WholeCondition = WholeCondition + ConditionText4
  	}
  
- 	WholeCondition = WholeCondition.replace(/\) /g, ')&&').replace(/.$/,"").replace(/.$/,"")
+ 	}
  
- 		var Filter = []
-		for (let x=0;x<NewTrancastionsArray.length;x++) {
-			var ComparableDate = new Date(NewTrancastionsArray[x].date);
-			if (eval(WholeCondition)) { 
-				Filter.unshift(NewTrancastionsArray[x])
-			}
-			else {
-				Filter.push(NewTrancastionsArray[x])
-			}
+ 
+ 	WholeCondition = WholeCondition.replace(/\) /g, ')&&').replace(/.$/,"").replace(/.$/,"")
+ 	if ( Condition1 == true && Condition2 == true) {
+ 		WholeCondition = WholeCondition.replace('&&' ,'||').replace(WholeCondition[0], WholeCondition[0]+"(").replace('"Расход")','"Расход"))' 	)
+ 	}
+
+ 	var Filter = []
+	for (let x=0;x<NewTrancastionsArray.length;x++) {
+		var ComparableDate = new Date(NewTrancastionsArray[x].date);
+		if (eval(WholeCondition)) { 
+			Filter.unshift(NewTrancastionsArray[x])
+		}
 	}
+
 	NewTrancastionsArray = Filter
+
+	if (ActiveFilters.length == 0) {
+ 		NewTrancastionsArray = data.results
+ 	}
+ 
 	return NewTrancastionsArray
 }
 
@@ -77,7 +93,7 @@ class App extends React.Component {
 	}
 
 	TableFilter=(e)=>{
- 
+ 		
 		this.setState({
       		TransactionsArray: TableFilters(e),
       		isShow:false
