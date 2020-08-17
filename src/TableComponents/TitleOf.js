07fa138039fb1d84bff56427bf7f57d46a44cfd5
Norpa1 <norpa344@gmail.com>
2020-08-17 21:22:11 +0300

    Update TitleOf.js

--- src/TableComponents/TitleOf.js
+++ src/TableComponents/TitleOf.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
 
-export default function TableRow( {todo, index} ) {
-	return (
-		<div>
-			<li>{todo}</li>
-		</div>
-		)
+const TableTitles = ["идентификатор транзакции","сумма транзакции","тип транзакции","дата транзакции"]
+ 
+export default function TableTitle() {
+    return (
+        <div className="container">
+            <div className="container-fluid">
+                {TableTitles.map((todo,idx) => {
+                    return <TileOf todo={todo} key={idx} index={idx}/>;
+                }
+                )}
+            </div>
+        </div>
+    );
+}
+
+
+function TileOf( {todo, index} ) {
+    return (
+        <div Style="background-color:#C84C41; font:25px verdana; border:0.5px solid #ddd; color:white" className="text-center col-3">{todo}</div>
+    );
 }
