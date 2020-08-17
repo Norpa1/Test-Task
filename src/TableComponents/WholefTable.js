b55585d78ba108c91f5d42aeae1176cd5939a928
Norpa1 <norpa344@gmail.com>
2020-08-17 21:22:45 +0300

    Update WholefTable.js

--- src/TableComponents/WholefTable.js
+++ src/TableComponents/WholefTable.js
@@ -1,31 +1,19 @@
-import React, {Component} from 'react';
-import TableRow from "./TileOfTable.js"
-import TileOf from "./TitleOf.js"
- 
+import React, {Component} from "react";
+import TableRow from "./TileOfTable.js";
 
-const TableTitles = ["идентификатор транзакции","сумма транзакции","тип транзакции","дата транзакции"]
  
  
 function Table({TransactionsArray}) {
-		return (
-		<div className="Container">
-
-		<div>
-			{TableTitles.map((todo,idx) => {
-				return <TileOf todo={todo} key={idx} index={idx}/>
-				}
-			)}
-		</div>
-
-		<div>
-			{TransactionsArray.map((todo,idx) => {
-				return <TableRow todo={todo} key={todo.id} index={idx}/>
-			}
-			
-			)}
-		</div>	
-		</div>	
-		)	
+    return (
+        <div className="table">
+            <div className="container position-static">
+                {TransactionsArray.map((todo,idx) => {
+                    return <TableRow todo={todo} key={todo.id} index={idx}/>;
+                }
+                )}
+            </div>	
+        </div>	
+    );	
 }
  
 	
