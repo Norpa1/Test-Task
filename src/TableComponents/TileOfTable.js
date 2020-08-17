c0a7a39b12a2e5c1aaa3417cd238aa505278a8eb
Norpa1 <norpa344@gmail.com>
2020-08-17 21:21:13 +0300

    Update TileOfTable.js

--- src/TableComponents/TileOfTable.js
+++ src/TableComponents/TileOfTable.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
 
 export default function TableRow( {todo, index} ) {
-	return (
-		<div>
-		<div className="TableTile">{todo.id}</div>
-		<div className="TableTile">{todo.value}</div>
-		<div className="TableTile">{todo.type}</div>
-		<div className="TableTile">{todo.date}</div>	 
-		 
-		</div>
-		)
+    return (
+        <div className="container-fluid">
+            <div Style="border:0.5px solid #ddd" className="col-3 font-weight-normal">{todo.id}</div>
+            <div Style="border:0.5px solid #ddd" className="col-3 font-weight-normal">{todo.value}</div>
+            <div Style="border:0.5px solid #ddd" className="col-3 font-weight-normal ">{todo.type}</div>
+            <div Style="border:0.5px solid #ddd" className="col-3 font-weight-normal">{todo.date}</div>	 
+        </div>
+    );
 }
